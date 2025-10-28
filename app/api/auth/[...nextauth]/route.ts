a7f63f5edc6daea3f15bbb43627d62959a18b7e1
ChristianJLC <christianjlc10@gmail.com>
2025-10-28 00:41:19 -0500

    fix: remove extra export from auth route for nextauth 4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import { compare } from "bcryptjs";
 
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     session: { strategy: "jwt" },
     secret: process.env.NEXTAUTH_SECRET,
     pages: { signIn: "/login" },
